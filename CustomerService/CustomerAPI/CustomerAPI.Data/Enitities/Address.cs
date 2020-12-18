ee2083e9d42270cea730fbd1b61a9065fc9463c5
Nirajkumar Lohbare <kumar.loh12345@gmail.com>
2020-12-18 04:00:08 -0500

    Data Layer, Entity Framework

---
+++ CustomerService/CustomerAPI/CustomerAPI.Data/Enitities/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CustomerApi.Data.Enitities
+{
+    public class Address
+    { 
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string AddressDetail { get; set; }
+
+
+        public int CustomerId { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public virtual Customer Customer { get; set; }
+
+        
+    }
+}
---
+++ CustomerService/CustomerAPI/CustomerAPI.Data/Enitities/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CustomerApi.Data.Enitities
+{    
+    public class Customer
+    {
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Birthday { get; set; }
+        public int? Age { get; set; }
+
+        public virtual ICollection<Address> CustomerAddress { get; set; }
+                    = new List<Address>();
+    }
+}
+
+
+public class Employee
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string EmployeeName { get; set; }
+    public string Skill { get; set; }
+    public string Email { get; set; }
+
+    public ICollection<Address> EmployeeAddress { get; set; }
+                = new List<Address>();
+}
